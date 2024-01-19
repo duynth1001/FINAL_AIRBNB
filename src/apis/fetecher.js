9c4cea4e05def6ad0d8be7d353711429a5f745c0
HoangDuy <duynth1001@gmail.com>
2024-01-19 17:52:32 +0700

    update

--- src/apis/fetecher.js
+++ src/apis/fetecher.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { CURRENT_USER } from "../constants";
 const fetcher = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: 'https://airbnbnew.cybersoft.edu.vn/api',
   headers: {
-    TokenCybersoft: import.meta.env.VITE_TOKEN,
+    TokenCybersoft: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MyIsIkhldEhhblN0cmluZyI6IjA1LzA1LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxNDg2NzIwMDAwMCIsIm5iZiI6MTY4Njc2MjAwMCwiZXhwIjoxNzE1MDE0ODAwfQ.5ch0U3B88fGDn067ipN5mT-pHyAOZTzdwpBiwr4p5Aw',
   },
 });
 fetcher.interceptors.request.use((config) => {
